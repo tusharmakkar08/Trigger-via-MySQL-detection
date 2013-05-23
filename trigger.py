dbfd72587c29af96b3af6455c46302f600991503
tusharmakkar08 <tusharmakkar08@gmail.com>
2013-05-23 13:20:31 +0530

    Trigger function implement

--- trigger.py
+++ trigger.py
@@ -3,6 +3,55 @@ import redis
 import MySQLdb as mdb
 import sys
 import time
+import random
+
+namelist=["Tushar Makkar","Tim Poston","Robert Frost","John \
+Kimberly","Jim Mathews","Henry Jacobson","Mark Salvador"]
+# Namelist of Students
+
+subjectlist=["Data Structures and programming","Unix Programming\
+","Microprocessors and Interfacing","Theory of Automation","Artificial\
+ Intelligence","Object Technology","Computer Networks"]
+# Names Of Subjects
+
+def trigger(no_trigger):
+	""" 
+	:param no_trigger: Number of triggers
+	"""
+	n=no_trigger
+	for i in xrange(n):
+		time.sleep(1)
+		# Causing Delays
+		
+		l=random.randint(1,500)
+		# Random Roll no. generation
+		
+		l1=random.randint(0,6)
+		# Random Name Generation
+		
+		l2=random.randint(0,6)
+		# Random Subject Generation
+		
+		l3=random.randint(1,10)
+		# Random Grade Generation
+		
+		l4=random.randint(0,1)
+		# Randomizing the triggers
+		
+		if l4==1:
+			print "Inserted Checking"
+		  # For  checking whether the trigger should be activated or not
+			
+			cur.execute("insert into student (id,name,subject,marks)\
+			 values (l,namelist[l1],subjectlist[l2],l3)")
+			 # Inserting into table
+		
+		cur.execute("DELIMITER $$\
+		CREATE TRIGGER inserting_trigger\
+		AFTER student INSERT \
+		sys_exec('python /home/ai/trigger/sptrial.py')")
+		
+		
 
 con=None
 
@@ -10,23 +59,35 @@ try:
 
     con=mdb.connect('localhost', 'testuser','test623', 'tio')		
     # Connecting to database
+    
     cur=con.cursor()
+    
     cur.execute("select * from student")
     # Executing Mysql command
+    
     data=cur.fetchall()
     # Fetching the answer
+    
     r_server = redis.Redis("localhost")
     # Connecting to localhost
+    
     k=len(data)
     # Number of data entries in table
+    
     for i in xrange(k):
         r_server.rpush("students",data[i])
 		# Pushing in students list
-    print r_server.lrange("students",0,-1)
     
+    print r_server.lrange("students",0,-1)
     # Printing from starting to end
+    
     r_server.delete("students")
     # Deleting the Reddis list 
+    
+    no_Trigger=input("Enter number of triggers you want to have ?")
+    
+    trigger(no_trigger)
+    # Calling trigger function
     
 except mdb.Error,e:
     
